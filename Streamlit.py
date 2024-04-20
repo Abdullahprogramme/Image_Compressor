b2b9e44ebf849316014f3aa6d974dd891ecc6b04
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2024-04-20 11:16:48 +0500

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -4,49 +4,59 @@ import tempfile
 from Main import main
 from io import BytesIO
 
-st.set_page_config(layout="centered")
+# Set page configuration
+st.set_page_config(page_title='QuadTree Image Compressor', layout="wide")
 
+
+# Helper function to convert size in bytes to appropriate unit
+def convert_size(size_bytes):
+    if size_bytes < 1024:
+        return f"{size_bytes} bytes"
+    elif size_bytes < 1048576:
+        return f"{size_bytes / 1024:.2f} KB"
+    else:
+        return f"{size_bytes / 1048576:.2f} MB"
+    
+# Define function for main application
 def Main():
+    # Sidebar
+    st.sidebar.title('Options')
+    compression_level = st.sidebar.radio("Compression Level", ("slightly less better", "slightly better",))
+
+    # Main content
     st.title('QuadTree Image Compressor')
 
-    compression_level = st.radio("Select a compression level", ("slightly less better", "slightly better",))
+    st.markdown("---")
 
-    if not compression_level:
-        st.error("Please select a compression type")
-        return
-    
+    # Image upload
     uploaded_file = st.file_uploader("Upload Image", type=["png", "jpg", "jpeg"])
 
     if uploaded_file and compression_level:
-        st.subheader('Original Image:')
+        st.subheader('Original Image')
         st.image(uploaded_file, caption='Original Image', use_column_width=True)
+        st.write('**Original Size:** ' + convert_size(len(uploaded_file.getvalue())))
 
-        original_size = len(uploaded_file.getvalue()) / 1024  # size in KB
-        st.write(f'Original size: {original_size:.2f} KB')
-
-        if st.button('Start'):
-        
-            st.write('Generating image... be patient, this may take a while.')
+        if st.button('Start Compression'):
+            st.write('**Generating compressed image...have patience, it may take a while!**')
             compressed_image = main(uploaded_file, compression_level)
-            st.write('Image generation complete.')
+            st.write('**Image Compression Complete!**')
 
             temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".png")
             compressed_image.save(temp_file.name)
 
-            st.subheader('Compressed Image:')
+            st.subheader('Compressed Image')
             st.image(temp_file.name, caption='Compressed Image', use_column_width=True)
-
-            compressed_size = os.path.getsize(temp_file.name) / 1024  # size in KB
-            st.write(f'Compressed size: {compressed_size:.2f} KB')
+            st.write('**Compressed Size:** ' + convert_size(os.path.getsize(temp_file.name)))
 
             # Convert the compressed image to binary data
             buffered = BytesIO()
             compressed_image.save(buffered, format="PNG")
             binary_data = buffered.getvalue()
 
-            # Display "Save" button
+            # Download button
+            st.markdown("---")
             st.download_button(label="Save Compressed Image", data=binary_data, file_name='compressed_image.png', mime='image/png')
 
-
+# Run the main application
 if __name__ == '__main__':
     Main()
